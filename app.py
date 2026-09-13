91c07380d31cc0ddef430116f4db4197eb8c0ac8
Saif Mohd <saifmohammed1746@gmail.com>
2026-09-13 18:08:37 +0530

    Fix Flask and docker binding, add docker file

--- app.py
+++ app.py
@@ -106,4 +106,4 @@ def delete_ticket(ticket_id):
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
